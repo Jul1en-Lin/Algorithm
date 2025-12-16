1622425b6254df1e0ee944b48cb3070bc8d5b889
Cary_C <1577045857@qq.com>
2025-12-17 00:25:36 +0800

    topK：最小K个数

--- J20251216/src/Solution.java
+++ J20251216/src/Solution.java
@@ -43,4 +43,40 @@ public class Solution {
         }
         return ret;
     }
+
+    // 快速排序
+    public int[] smallestK3(int[] arr, int k) {
+        quickSort(arr, 0, arr.length - 1, k);
+        int[] ret = new int[k];
+        for (int i = 0; i < k; i++)
+            ret[i] = arr[i];
+        return ret;
+    }
+
+    public void quickSort(int[] num, int l, int r, int k) {
+        if (r - l + 1 == k) return;
+        // 选取随机元素
+        int index = l + new Random().nextInt(r - l + 1);
+        int key = num[index];
+        // 定义指针 + 分区
+        int left = l - 1,right = r + 1;
+        for (int cur = l;cur < right; ) {
+            if (num[cur] > key) swap(num, cur, --right);
+            else if (num[cur] < key) swap(num, cur++, ++left);
+            else cur++;
+        }
+
+        // 分类讨论
+        // [l, left] [left + 1, right - 1] [right, r]
+        int a = left - l + 1,b = right - left - 1,c = r - right + 1;
+        if (a >= k) quickSort(num, l, left, k);// a == k 的情况会在quickSort的if (r - l + 1 == k)条件中 return
+        else if (a + b >= k) return;
+        else quickSort(num, right, r, k - a - b);
+    }
+
+    public void swap(int[] num, int x, int y) {
+        int tmp = num[x];
+        num[x] = num[y];
+        num[y] = tmp;
+    }
 }
